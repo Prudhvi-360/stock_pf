f08a33f7e921120f928e2967888737713a7d1461
Prudhvi K <kolleparaprudhvi@gmail.com>
2026-05-14 18:05:13 +0000

    added totret

--- srv/services.js
+++ srv/services.js
@@ -7,27 +7,34 @@ module.exports = cds.service.impl(async function () {
       if (quantity <= 0) {
          req.error(400, 'Quantity must be greater than 0');
       }
-      //const exists = await SELECT.one.from(stocks).where({ symbol : symbol });
-      // console.log(raw);
-      // if (exists) {
-      //    req.error(400, 'Stock already exists');         
-      // }
+      const exists = await SELECT.one.from(stockset).where({ symbol});
+      //console.log(exists);
+      if (exists) {
+          req.error(400, 'Stock already exists');         
+       }
       req.data.invested = quantity * avgprice;
       req.data.current = quantity * mktprice;
       req.data.totreturns = ((req.data.current - req.data.invested) / req.data.invested) * 100;
    });
 
    this.after('READ', 'stockset', async (data, res) => {
-      // const { symbol, name, quantity, avgprice } = res?.results[0];
-      console.log(data);
-      // res.results[0].invested = quantity * avgprice;
       data.forEach((item, index) => {
          //res.results[index].invested = item.quantity * item.avgprice;
          //res.results[index].current = item.quantity * item.mktprice;
          data[index].invested = item.quantity * item.avgprice;
          data[index].current = item.quantity * item.mktprice;
+         data[index].totreturns = ((data[index].current - data[index].invested) / data[index].invested) * 100 + '%';
       });
 
+   })
+
+   this.before('DELETE', 'stockset', async (req) => {
+      console.log(req.params[0]);
+      const { id } = req.params[0];
+      const exists = await SELECT.one.from(stockset).where({ id });
+      if (!exists) {
+         req.error(404, 'Stock not found');
+      }
    })
 
    this.after('READ', 'holdings', async (data) => {
